6741a0487a38c4d4db2a16bf937509cc8e055023
CodeStoopid <145154166+CodeStoopid@users.noreply.github.com>
2024-01-11 08:38:15 -0600

    Update and rename scrapematest.py to scrapemawip.py

--- scrapemawip.py
+++ scrapemawip.py
@@ -21,6 +21,3 @@ while exist == 0:
 if exist > 0:
     webbrowser.open_new_tab('file:///C:/Users/100038391/OneDrive%20-%20Clear%20Creek%20ISD/Desktop/Scrapema%20stuff/Scrapema-main/YorkAuctionSite/yorkAuctions.htm')
 
-#turn website info into a string
-#make for loop to check for when string has available set to true
-#when for loop is triggered open the web page
