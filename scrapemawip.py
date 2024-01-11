802f947cb1ebe3b62db952dc22a8926938ca2b7d
CodeStoopid <145154166+CodeStoopid@users.noreply.github.com>
2024-01-11 08:51:29 -0600

    Update scrapemawip.py
    shortened code by removing unnecessary if-statement and relocating its contents.

--- scrapemawip.py
+++ scrapemawip.py
@@ -8,16 +8,15 @@ pagelen = len(pageinfo)
 trig = bytes('available', 'UTF-8')
 
 triglen = len(trig)
-exist = 0
+check = 0
 
-while exist == 0:
+while check == 0:
     for x in range(0, pagelen - triglen):
         phrase = pageinfo[x:x + triglen]
     
     
         if phrase == trig:
-            exist += 1
+            webbrowser.open_new_tab('file:///C:/Users/100038391/OneDrive%20-%20Clear%20Creek%20ISD/Desktop/Scrapema%20stuff/Scrapema-main/YorkAuctionSite/yorkAuctions.htm')
+            check += 1
 
-if exist > 0:
-    webbrowser.open_new_tab('file:///C:/Users/100038391/OneDrive%20-%20Clear%20Creek%20ISD/Desktop/Scrapema%20stuff/Scrapema-main/YorkAuctionSite/yorkAuctions.htm')
 
