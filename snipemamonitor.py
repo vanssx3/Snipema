b00f1c0dbdf56eb9cf8a2f5963dffb970cf08962
CodeStoopid <145154166+CodeStoopid@users.noreply.github.com>
2024-01-17 13:06:00 -0600

    Update snipemamonitor.py

--- snipemamonitor.py
+++ snipemamonitor.py
@@ -1,36 +1,14 @@
-from pathlib import Path
-import pynput
-import os
+
+
 import webbrowser
-import pyautogui
-import time
-import pyperclip
+
 import urllib.request
 
-siteloc = Path(__file__).with_name('YorkAuctionSite')
-website = Path(__file__).with_name('yorkAuctions.htm')
-print(website)
-os.chdir(siteloc)
-
-webbrowser.open('yorkAuctions.htm')
-
-mouse_listener = pynput.mouse.Listener(suppress=True)
-mouse_listener.start()
-keyboard_listener = pynput.keyboard.Listener(suppress=True)
-keyboard_listener.start()
-
-time.sleep(5)
-
-mouse_listener.stop()
-keyboard_listener.stop()
-
-pyautogui.hotkey('ctrl', 'l')
-pyautogui.hotkey('ctrl', 'c')
-pyautogui.hotkey('alt', 'esc')
-data = pyperclip.paste()
+data = 'https://vanssx3.github.io/Snipema/'
 
 page = urllib.request.urlopen(data)
 pageinfo = page.read()
+
 
 pagelen = len(pageinfo)
 trig = bytes('available', 'UTF-8')
