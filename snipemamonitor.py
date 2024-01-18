b531288c3b6de27327013a94ac95483131046680
CodeStoopid <145154166+CodeStoopid@users.noreply.github.com>
2024-01-18 08:02:04 -0600

    Update snipemamonitor.py
    added comments

--- snipemamonitor.py
+++ snipemamonitor.py
@@ -1,31 +1,43 @@
-
-
+#imports
 import webbrowser
-
 import urllib.request
 
-data = 'https://vanssx3.github.io/Snipema/'
+#storing website url
+web = 'https://vanssx3.github.io/Snipema/'
 
-page = urllib.request.urlopen(data)
-pageinfo = page.read()
-
-
-pagelen = len(pageinfo)
+#sets trigger phrase and converts it into bytes
 trig = bytes('available', 'UTF-8')
 
+#gets length of encoded trigger phrase
 triglen = len(trig)
+
+#stores initial value for while-loop condition
 check = 0
 
+#initializes while-loop 
 while check == 0:
-    page = urllib.request.urlopen(data)
+    
+    #requests data from url
+    page = urllib.request.urlopen(web)
+    
+    #stores data from url as bytes
     pageinfo = page.read()
 
+    #stores length of provided data
     pagelen = len(pageinfo)
+    
+    #initializes for-loop traversing the url data
     for x in range(0, pagelen - triglen):
+        
+        #gets data between x and x + trigger length
         phrase = pageinfo[x:x + triglen]
-    
-    
-        if phrase == trig:
-            webbrowser.open_new_tab(data)
-            check += 1
 
+        #checks if data chunk is 
+        if phrase == trig:
+
+            #opens url in new tab
+            webbrowser.open_new_tab(web)
+            
+            #makes the while-condition false 
+            check += 1
+#automatic product sniper coming soon
