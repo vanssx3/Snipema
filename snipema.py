d0787599bd9de661eac5341065a542560f67f2ed
CodeStoopid <145154166+CodeStoopid@users.noreply.github.com>
2024-01-19 07:44:24 -0600

    Update and rename snipemamonitor.py to snipema.py

--- snipema.py
+++ snipema.py
@@ -1,19 +1,30 @@
 #imports
 import webbrowser
 import urllib.request
+import sys
+
 
 #storing website url
 web = 'https://vanssx3.github.io/Snipema/'
 
 #sets trigger phrase and converts it into bytes
-trig = bytes('available', 'UTF-8')
+trig = bytes('">BUY NOW!!</button', 'UTF-8')
 
 #gets length of encoded trigger phrase
 triglen = len(trig)
-
+extra = False
 #stores initial value for while-loop condition
 check = 0
-
+args = len(sys.argv)
+print(args)
+if args > 1:
+    arg1 = sys.argv[1]
+    
+    if args > 2:
+        arg2 = sys.argv[2]
+        extra = True
+else:
+    arg1 = " "
 #initializes while-loop 
 while check == 0:
     
@@ -36,8 +47,19 @@ while check == 0:
         if phrase == trig:
 
             #opens url in new tab
-            webbrowser.open_new_tab(web)
+            if extra == True:
+                if arg1 == '-w':
+                    if arg2 == '-c':
+                        webbrowser.get('"C:/Program Files/Google/Chrome/Application/chrome.exe" %s').open(web)
+                    if arg2 == '-e':
+                        webbrowser.get('"C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe" %s').open(web)
+                if arg1 == '-l':
+                    if arg2 == '-c':
+                        webbrowser.get('/usr/bin/google-chrome').open(web)
+                    if arg2 == '-f':
+                        webbrowser.get('/usr/bin/firefox').open(web)
+            else:
+                print('Welcome to Snipema.py')
             
             #makes the while-condition false 
             check += 1
-#automatic product sniper coming soon
