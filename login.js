1241176d16d7c88cd29044f4b9145d23d39fc938
vanssx3 <144460118+vanssx3@users.noreply.github.com>
2024-01-19 08:58:31 -0600

    Add files via upload

---
+++ login.js
@@ -0,0 +1,10 @@
+//User login variables
+var username = "";
+var email = "";
+var password = "";
+
+//Users that can be logged into (too lazy to use php :p)
+const users = ["Yorkiplier", "slimjimgod", "mrVampork96"]
+const emails = ["theyorker@gmail.com", "davidavid@gmail.com", "therealvampork@vampire.com"]
+const passwords = ["H3110everyone", "cant.teleport", "ColdRoom123"]
+
